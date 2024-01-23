d12d8040f6578d4881f9795fd3bfc39e09f96748
s-shahriargit <syedshahriar99@gmail.com>
2024-01-23 21:07:14 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: { 'manrope': ['Manrope', 'sans-serif'],},
+    },
+    plugins: [],
+  }
